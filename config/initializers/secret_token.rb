e009fc1a7640d994ba0f3774a8b3c83e18f155aa
Pedro Maltez <maltezpedro@gmail.com>
2013-04-11 11:09:52 +0100

    Generate rails app structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsMovieApp::Application.config.secret_token = '1c8893f47084eac4866598c5a49a6430713c223a772f5bb14398602c8e9a04e90168be86c46bbfaa151635be64e0c84ea1e9e11219d0e06e823a5e3f909912da'
